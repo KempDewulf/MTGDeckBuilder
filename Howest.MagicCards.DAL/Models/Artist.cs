6c111e596bdb375f28b7c332aaef63e7d9faf823
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-04-30 21:32:03 +0200

    add models from db, with context

--- Howest.MagicCards.DAL/Models/Artist.cs
+++ Howest.MagicCards.DAL/Models/Artist.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Howest.MagicCards.DAL.Models;
 
+[Table("artists")]
 public partial class Artist
 {
+    [Key]
+    [Column("id")]
     public long Id { get; set; }
 
+    [Required]
+    [Column("full_name")]
+    [StringLength(255)]
     public string FullName { get; set; }
 
+    [Column("created_at", TypeName = "datetime")]
     public DateTime? CreatedAt { get; set; }
 
+    [Column("updated_at", TypeName = "datetime")]
     public DateTime? UpdatedAt { get; set; }
 
+    [InverseProperty("Artist")]
     public virtual ICollection<Card> Cards { get; set; } = new List<Card>();
 }
