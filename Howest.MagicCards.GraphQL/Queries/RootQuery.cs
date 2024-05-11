7865a64865aaeccaca71c1dde5f259ee85f96c57
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-05-12 00:45:02 +0200

    add grapql root files

---
+++ Howest.MagicCards.GraphQL/Queries/RootQuery.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.Types;
+using Howest.MagicCards.DAL.Repositories;
+using Howest.MagicCards.GraphQL.Types;
+using SharpCompress.Crypto;
+
+namespace Howest.MagicCards.GraphQL.Queries;
+
+public class RootQuery : ObjectGraphType
+{
+    public RootQuery(ICardRepository cardRepository)
+    {
+        Field<ListGraphType<CardType>>("cards")
+            .Resolve(context => cardRepository.GetAllCards());
+    }
+    
+}
---
+++ Howest.MagicCards.GraphQL/Schemas/RootSchema.cs
@@ -0,0 +1,12 @@
+﻿using GraphQL.Types;
+using Howest.MagicCards.GraphQL.Queries;
+
+namespace Howest.MagicCards.GraphQL.Schemas;
+
+public class RootSchema : Schema
+{
+    public RootSchema(IServiceProvider serviceProvider) : base(serviceProvider)
+    {
+        Query = serviceProvider.GetRequiredService<RootQuery>();
+    }
+}
