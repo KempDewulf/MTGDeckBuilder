5bdead80d3297f261c5073ef0993bf018e1266d5
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-05-12 02:18:41 +0200

    add artist repository

---
+++ Howest.MagicCards.DAL/Repositories/IArtistRepository.cs
@@ -0,0 +1,10 @@
+﻿using Howest.MagicCards.DAL.Models;
+
+namespace Howest.MagicCards.DAL.Repositories;
+
+public interface IArtistRepository
+{
+    IQueryable<Artist> GetAllArtists();
+    
+    IQueryable<Artist> GetArtistFromCardId(int cardId);
+}
---
+++ Howest.MagicCards.DAL/Repositories/SqlArtistRepository.cs
@@ -0,0 +1,26 @@
+﻿using Howest.MagicCards.DAL.Models;
+
+namespace Howest.MagicCards.DAL.Repositories;
+
+public class SqlArtistRepository : IArtistRepository
+{
+    private readonly MtgContext _context;
+    
+    public SqlArtistRepository(MtgContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+    
+    public IQueryable<Artist> GetAllArtists()
+    {
+        return _context.Artists
+            .Select(a => a);   
+    }
+
+    public IQueryable<Artist> GetArtistFromCardId(int cardId)
+    {
+        return _context.Artists
+            .Where(a => a.Cards.Any(c => c.Id == cardId))
+            .Select(a => a);
+    }
+}
